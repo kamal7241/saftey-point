cfaa877a363e398ca9416d408caa30991ea19dbb
George Saad <georgesaad111@gmail.com>
2024-12-15 01:56:27 +0200

    fixes

--- src/app/[locale]/dashboard/admin-management/manage-admins/page.tsx
+++ src/app/[locale]/dashboard/admin-management/manage-admins/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 export default function Page() {
   
   return <h1>manage-admins</h1>
--- src/app/[locale]/dashboard/admin-management/roles-permissions/page.tsx
+++ src/app/[locale]/dashboard/admin-management/roles-permissions/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 export default function Page() {
   
   return <h1>roles-permissions</h1>
